efa58792b14e602a599c4fca92acc1edc9ba43ae
thanmai kamalapuram <thanmaikamalapuram@thanmais-MacBook-Air.local>
2025-10-30 19:42:39 +0530

    last

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .scss
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link_tree ../../javascript/controllers .js//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
